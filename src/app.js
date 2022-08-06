7365eeb6790976c1d2afc55d0ef7d67cdbefaf21
diadieva <110339735+diadieva@users.noreply.github.com>
2022-08-06 22:22:09 +0200

    add wind, humidity, description

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp)
-cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "67463d90019f3c15672ebddc2b82fac3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=50.43331&lon=30.5167&appid=${apiKey}&units=metric`;
